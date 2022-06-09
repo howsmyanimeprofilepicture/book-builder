23d3ca09226c30e4734169b9725cf6009fc3668f
howsmyanimeprofilepicture <howsmyanimeprofilepicture@gmail.com>
2022-06-09 09:06:19 +0900

    test-1

--- src/builder/builder.ts
+++ src/builder/builder.ts
@@ -11,26 +11,7 @@ import createCategoryStyle from './createCategoryStyle';
 
 
 export default function builder(): void {
-    T.makeDir(path.join(__dirname, "../../docs"));
-    T.makeDir(path.join(__dirname, "../../docs", "assets"));
-    T.makeDir(path.join(__dirname, "../../docs", "posts"));
-
-    let maincategory_html: string = ""
-
-    const mainChapters: Array<string> = fs.readdirSync(
-        path.join(__dirname, "../posts")
-    );
-
-    mainChapters.forEach((mainChapter) => {
-        T.makeDir(path.join(
-            __dirname, "../../docs", "posts", mainChapter,
-        ));
-        const maincategory: Maincategory = new Maincategory({
-            categoryName: mainChapter,
-            subcategories: createSubcategories(mainChapter),
-        })
-        maincategory_html += maincategory.getHTML()
-    })
+   
 
     const config: any = T.getConfig();
     const { main, sub } = config.categoryStyle
--- src/builder/createSubcategories.ts
+++ src/builder/createSubcategories.ts
@@ -4,35 +4,95 @@ import parseMarkdown from "./parseMarkdown";
 import Subcategory from "../Elements/Subcategory";
 import path from "path";
 
-export default function createSubcategories(mainChapter: string): Array<Subcategory> {
-    const subChapters: Array<string> = fs.readdirSync(
-        path.join(__dirname, "../posts", mainChapter));
+import Maincategory from "../Elements/Maincategory";
+import assert from "assert";
 
-    return subChapters.map((subChapter) => {
-        T.makeDir(path.join(__dirname, "../../docs/posts",
-            mainChapter, subChapter));
-        let posts: Array<string> = fs.readdirSync(
-            path.join(__dirname, "../posts", mainChapter, subChapter)
+export class CategoryFacotry {
+    constructor(public directoryDict: any = {}) {
+        T.makeDir(path.join(__dirname, "../../docs"));
+        T.makeDir(path.join(__dirname, "../../docs", "assets"));
+        T.makeDir(path.join(__dirname, "../../docs", "posts"));
+    }
+
+    public parseMarkdowns(): void {
+        this.recursiveParser(this.directoryDict);
+    }
+
+
+    private recursiveParser(directoryDict: any, opt_dir: string = "") {
+        assert(!Array.isArray(directoryDict), "`dirmap`'s type must not be `Array`!!💩")
+
+        for (let dir in directoryDict) {
+            T.makeDir(path.join(__dirname, "../../docs/posts", opt_dir, dir))
+            if (Array.isArray(directoryDict[dir])) {
+                this.parseMarkdownFiles(directoryDict[dir], path.join(opt_dir, dir));
+            } else {
+                this.recursiveParser(directoryDict[dir], opt_dir + dir)
+            }
+        }
+    }
+
+    private parseMarkdownFiles(
+        markdowns: Array<string>,
+        dir: string = "",
+        verbose: boolean = true,
+    ) {
+        markdowns.forEach((file) => {
+            if (file.endsWith(".md")) {
+                const parsedHTML: string = parseMarkdown(fs.readFileSync(
+                    path.join(__dirname, "../posts", dir, file),
+                    { encoding: "utf-8" },
+                ));
+
+                fs.writeFileSync(
+                    path.join(
+                        __dirname, "../../docs/posts",
+                        dir, file.split(".md")[0] + ".html"
+                    ),
+                    parsedHTML,
+                    { encoding: "utf-8" },
+                );
+
+                if (verbose) {
+                    console.log(`${path.join(
+                        __dirname, "../../docs/posts",
+                        dir, file.split(".md")[0] + ".html"
+                    )}:🐤🐤 parsing complete!`);
+                }
+            }
+        })
+    }
+}
+
+// T.makeDir(path.join(
+//     __dirname, "../../docs", "posts", part,
+// ));
+
+
+class AutoCategoryFactory extends CategoryFacotry {
+
+    public setdirectoryDictAuto(): void {
+        this.directoryDict = {};
+
+        const parts: Array<string> = fs.readdirSync(
+            path.join(__dirname, "../posts")
         );
-        posts = posts.map(post => post.split(".md")[0]);
 
-        posts.forEach((post) => {
-            let mdText: string = fs.readFileSync(
-                path.join(__dirname, `../posts/${mainChapter}/${subChapter}/${post}.md`),
-                { encoding: "utf-8" }
+        parts.forEach((part) => {
+            const chapters: Array<string> = fs.readdirSync(
+                path.join(__dirname, "../posts", part),
             );
-            mdText = parseMarkdown(mdText);
-
-            fs.writeFileSync(
-                path.join(__dirname, `../../docs/posts/${mainChapter}/${subChapter}/${post}.html`),
-                mdText, {
-                encoding: "utf-8"
+            chapters.forEach((chpater) => {
+                this.directoryDict[part][chpater] = [];
+                const files: Array<string> = fs.readdirSync(
+                    path.join(__dirname, "../posts", part, chpater)
+                );
+                files.forEach((file) => {
+                    if (file.endsWith(".md")) {
+                        this.directoryDict[part][chpater].push(file);
+                    }
+                });
             })
         })
-
-        return new Subcategory({
-            categoryName: subChapter,
-            posts: posts,
-        });
-    })
+    }
 }
