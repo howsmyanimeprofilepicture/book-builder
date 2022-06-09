9d2d33d6ae711ac827c9af46505571ba25dbb9a7
howsmyanimeprofilepicture <howsmyanimeprofilepicture@gmail.com>
2022-06-09 17:05:27 +0900

    mod Path
    1. convert Path interface to Path abstract class

--- src/Path/Path.ts
+++ src/Path/Path.ts
@@ -1,23 +1,29 @@
 import path from "path";
 
-export interface Path {
-    path: string;
-    basepath: string;
+export abstract class Path {
+    abstract path: string;
+    abstract basepath: string;
+
+    public getPath(): string {
+        return path.join(this.basepath, this.path);
+    }
 }
 
 
-export class OriginPath implements Path {
+export class OriginPath extends Path {
     public basepath: string = path.join(__dirname, "../");
     public path: string;
     constructor(_path: string = "posts", ...paths: string[]) {
+        super();
         this.path = path.join(_path, ...paths)
     }
 }
 
-export class DestPath implements Path {
+export class DestPath extends Path {
     public basepath: string = path.join(__dirname, "../../");
     public path: string;
     constructor(_path: string = "docs", ...paths: string[]) {
+        super();
         this.path = path.join(_path, ...paths)
     }
 }
